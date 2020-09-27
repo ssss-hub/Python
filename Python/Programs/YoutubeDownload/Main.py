9078921edd19cd6f8318c001e43ad54a1c9baba0
大平真大 <Sersffccfe@gmail.com>
2020-09-27 23:24:11 +0900

    ひとまず完成(ただしLibraryを読み込むことで動作させたい)

--- Python/Programs/YoutubeDownload/Main.py
+++ Python/Programs/YoutubeDownload/Main.py
@@ -1,32 +1,81 @@
 from tqdm import tqdm
 import os
-import Library
-import sys
-sys.path.append('/content/drive/My Drive/Colab Notebooks/Personal_Library/')
 
 
 def Delete_Text(text_path, text):
-    tmp = Library.Get_Text_List(text_path)
-    Library.Write_Text(text_path, '', True)
+    tmp = Get_Text_List(text_path)
+    Write_Text(text_path, '')
     for i in tmp:
         if i != text:
-            Library.Write_Text(text_path, i)
+            Write_Text(text_path, i)
+
+
+def Write_Text(text_path, text):
+    with open(text_path, 'w', encoding='utf-8') as f:
+        f.write(text)
+
+
+def Get_Text_List(text_path):
+    texts = Get_Text(text_path)
+
+    tmp = []
+    for text in texts.split('\n'):
+        if text.strip() != '':
+            tmp.append(text.strip())
+
+    return tmp
+
+
+def Get_Text(text_path):
+    with open(text_path, 'r', encoding='utf-8') as f:
+        texts = f.readlines()
+
+    tmp = ''
+    for text in texts:
+        text = text.strip()
+        if text != '':
+            tmp = tmp + text + '\n'
+
+    tmp = tmp.strip()
+    return tmp
+
+
+def Download_Video(URL, folder_name):
+    import os
+    import youtube_dl
+
+    os.makedirs(folder_name, exist_ok=True)
+    before_folder = os.path.dirname(__file__)
+    os.chdir(folder_name)
+    isSucceed = True
+    try:
+        ydl_opts = {'outtmpl': '%(title)s.%(ext)s',
+                    'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]', '--merge-output-format': 'mkv'}
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            result = ydl.extract_info(URL, download=True)
+    except:
+        try:
+            ydl_opts = {'outtmpl': '%(title)s.%(ext)s', 'format': 'best'}
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                result = ydl.extract_info(URL, download=True)
+        except:
+            isSucceed = False
+
+    os.chdir(before_folder)
+
+    return isSucceed
 
 
 urls = set()
-url_text = os.path.join(os.getcwd(), 'URL.txt')
+url_text = os.path.join(os.path.dirname(__file__), 'URL.txt')
 
-if len(Library.Get_Text(url_text)) == 0:
-    Library.Write_Text(url_text, input('URLを入力してください。'), True)
+if len(Get_Text(url_text)) == 0:
+    Write_Text(url_text, input('URLを入力してください。'), True)
 
-if input('画像化しますか？(y or n)') == 'y':
-    make_photo = True
-else:
-    make_photo = False
-
-for i in tqdm(Library.Get_Text_List(url_text)):
-    Library.Youtube_Download(i, os.path.join(
-        os.getcwd(), 'Download_Files'), make_photo)
-    Delete_Text(url_text, i)
+for i in tqdm(Get_Text_List(url_text)):
+    isSucceed = Download_Video(i, os.path.join(
+        os.path.dirname(__file__), 'Download_Files'))
+    if isSucceed:
+        Delete_Text(url_text, i)
 
 print('終了')
